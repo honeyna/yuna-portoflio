c3e1fb0f20185668c830177a8d255f32387ae34f
honeyna <yundutls@gmail.com>
2023-05-21 22:33:43 +0900

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // video
 $(document).ready(function() {
-    $(".myvideos").on("mouseout", function(event) {
-      this.play();
-  
-    }).on('mouseover', function(event) {
-      this.pause();
-  
-    });
-  })
+  $(".myvideos").on("mouseout", function(event) {
+    this.play();
+
+  }).on('mouseover', function(event) {
+    this.pause();
+
+  });
+})
+
