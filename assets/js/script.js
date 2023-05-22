01ae15ea016e9869dd8e98189d22fcc15c1a7b14
honeyna <yundutls@gmail.com>
2023-05-22 19:31:55 +0900

    projcet commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+//header
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 // video
 $(document).ready(function() {
   $(".myvideos").on("mouseout", function(event) {
