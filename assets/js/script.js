18daca66bc951259719f6388f28384be44bd3c69
honeyna <yundutls@gmail.com>
2023-05-31 10:15:43 +0900

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-//header
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("header");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 // video
 $(document).ready(function() {
   $(".myvideos").on("mouseout", function(event) {
@@ -23,4 +8,3 @@ $(document).ready(function() {
 
   });
 })
-
